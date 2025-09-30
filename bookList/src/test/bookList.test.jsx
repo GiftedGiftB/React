1b3bb521674df25a554bb1ff894b15ea28991bac
GiftedGiftB <asekhamegift@gmail.com>
2025-09-30 14:23:06 +0100

    first commit

---
+++ bookList/src/test/bookList.test.jsx
@@ -0,0 +1,23 @@
+import {test, expect, beforeEach, afterEach} from "vitest";
+import {cleanup, fireEvent, render, screen} from "@testing-library/react";
+import BookList from "../component/BookList";
+
+beforeEach(() => {
+    render(<BookList/>)
+})
+
+afterEach(() => {
+    cleanup()
+})
+
+test("that the booklist rendered on the screen", () =>{
+    const listItem = screen.getAllByRole("listitem");
+    expect(listItem.length).toBe(4);
+})
+
+test("that I can delete a book", () => {
+    const deleteBtn = screen.getAllByText(/delete/i)
+    fireEvent.click(deleteBtn[0]);
+    const listItem = screen.getAllByRole("listitem");
+    expect(listItem.length).toBe(3);
+})
